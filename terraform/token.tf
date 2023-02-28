8e12e9ed1e37fd1b6b1e0864a02a8bf31d6b8d8f
antipovs <antipovs@yandex-team.ru>
2023-02-28 16:37:01 +0100

    Set terraform

--- terraform/token.tf
+++
@@ -1,3 +0,0 @@
-variable "token" {
-  default = "t1.9euelZrGns-TjImXlpTLnpWWjM6dj-3rnpWax5KQlJjNmpuTkMnLy4-MkI3l8_dKRQdg-e8qfCov_t3z90I3B2D57yp8Ki_-.h_1ouJrR-3xxoPlmuMfeCOeYxEmFDzjft0Zkid9QfER2YYhMu96Fs_VZAN35NQKCLJBDgUEYLePjAKUuRXvuCw"
-}
