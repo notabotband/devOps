e15c87a45690567167c5d72224a0c4478f2b37d1
antipovs <antipovs@yandex-team.ru>
2023-02-28 16:34:23 +0100

    Set terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  default = "chapter5-task2-std-013-59"
+}
+variable "image_id" {
+  default = "fd8cqj9qiedndmmi3vq6"
+}
+
+variable "subnet_id" {
+  description = "CIDR block for the subnet"
+  default = "e9bi95vd74f8lp1mgdvm"
+}
+
+variable "cloud_id" {
+  default = "b1g3jddf4nv5e9okle7p"
+}
+
+variable "folder_id" {
+  default = "b1g2m7136g982ot032pf"
+}
+
+variable "zone" {
+  default = "ru-central1-a"
+}
+
